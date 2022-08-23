e36f71162b7c0e570a1bb29d119d689c5ef3b684
Merkushev <igor.merkushev@scalable.capital>
2022-08-23 18:50:22 +0200

    add github actions

---
+++ api/src/main/kotlin/space/geek/ktassets/api/AssetsController.kt
@@ -0,0 +1,16 @@
+package space.geek.ktassets.api
+
+import mu.KotlinLogging
+import org.springframework.web.bind.annotation.RestController
+import space.geek.ktassets.appilcation.asset.AssetService
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+internal class AssetsController(
+    private val assetService: AssetService
+) {
+
+
+
+}
