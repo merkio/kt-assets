724a297a8e9196c220b25780c5c8afa1803d3c47
Merkushev <igor.merkushev@scalable.capital>
2022-08-25 12:34:17 +0200

    fix sonar code coverage

--- messaging/src/test/kotlin/space/geek/ktassets/messaging/Fixtures.kt
+++ messaging/src/test/kotlin/space/geek/ktassets/messaging/Fixtures.kt
@@ -1,7 +1,26 @@
 package space.geek.ktassets.messaging
 
+import space.geek.ktassets.domain.asset.Asset
+import space.geek.ktassets.messaging.processor.asset.AssetMessage
+import java.time.OffsetDateTime
+
 object Fixtures {
 
     val CONSUMER_GROUP_ID = "assets-group-id"
     val CONSUMER_TOPIC = "assets"
+
+    val ASSET_MESSAGE = AssetMessage(
+        key = "key",
+        url = "http://localhost"
+    )
+    val ASSET = Asset(
+        id = 1,
+        key = "key",
+        url = "http://localhost",
+        username = "user",
+        createdAt = OffsetDateTime.now(),
+        updatedAt = OffsetDateTime.now(),
+        tags = emptyMap(),
+        metadata = emptyMap()
+    )
 }
